d5cb176f663a7a71314280df579987f775407704
Langerz82 <langley.joshua@gmail.com>
2023-02-14 19:22:16 +1100

    t12

--- yabause/src/peripheral.c
+++ yabause/src/peripheral.c
@@ -213,6 +213,7 @@ void PerPadLeftReleased(PerPad_struct * pad) {
 //////////////////////////////////////////////////////////////////////////////
 
 void PerPadStartPressed(PerPad_struct * pad) {
+	 print("PerPadStartPressed.");
    *pad->padbits &= 0xF7;
    SMPCLOG("Start\n");
 }
